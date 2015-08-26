f9af16c85cdeac39a89032e38f748fa34aec5112
Arianit Uka <arianitu@gmail.com>
2015-08-25 21:28:47 -0400

    All of the files for the template

---
+++ app/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import AppContainer from "./AppContainer";
+
+React.render(
+  <AppContainer />,
+  document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    context: __dirname + "/app",
+    entry: {
+        javascript: "./App.js",
+        html: "./index.html"
+    },
+    
+    output: {
+        filename: "App.js",
+        path: __dirname + "/dist"
+    },
+    
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/ },
+            { test: /\.html$/, loader: "file?name=[name].[ext]" },
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json'] 
+    }
+};
